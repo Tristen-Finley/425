35412e6193971d6110fa527394b249aab759b20e
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2020-10-27 21:30:30 -0500

    Update scrapper.py
    script works now, also handles api errors so it doesn't fail

--- scrapper.py
+++ scrapper.py
@@ -3,23 +3,29 @@ from datetime import datetime
 import time
 
 subs = ['lemonjelly'] #list of subs to pull from (large subs take forever)
-day = '30' #how many days back we look
-url = "https://api.pushshift.io/reddit/{}/search/?subreddit={}&limit=1000&sort=desc&after={}d&before="
+day = '1' #how many days back we look
+url = "https://api.pushshift.io/reddit/{}/search/?subreddit={}&size=100&sort=desc&after={}d&before="
 object_type = ["submission", "comment"]
 start_time = datetime.utcnow()
 
 for subreddit in subs:
-    
-    count = 0
-    prev_epoch = int(start_time.timestamp())
+
+    nlocked=0
+    nremoved=0
     submissiondata = []
     commentdata = []
     for s in object_type:
+        prev_epoch = int(start_time.timestamp())
         while True:
+            time.sleep(1) #have to wait inbetween api calls so there is no errors
             new_url = url.format(s, subreddit, day)+str(prev_epoch)
             res = requests.get(new_url)
             print(new_url)
-            dat = res.json()['data']
+            try:
+                dat = res.json()['data']
+            except ValueError:
+                print("API Error, trying again")
+                continue
             if(len(dat) == 0):
                 break
             if(s == 'comment'):
@@ -27,6 +33,17 @@ for subreddit in subs:
             elif(s == 'submission'):
                 submissiondata.extend(dat)
             prev_epoch=dat[-1]['created_utc']
-    
+
+    for post in submissiondata:
+        if(post['locked'] == True):
+            nlocked+=1;
+
+    for comment in commentdata:
+        if(comment['author'] == 'removed'):
+            nremoved += 1
+
+    print("Number of locked posts: {}".format(nlocked))
+    print("Number of removed comments: {}".format(nremoved))
+
 print(len(submissiondata))
 print(len(commentdata))
